42bc53bc768d01e2b2ea964129c1845f4f5ebced
Navdeep Singh <navdeepddn.97@gmail.com>
2018-02-09 05:15:06 +0530

    second commit working on local user login

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://<navdeep>:<social_ado>@ds229918.mlab.com:29918/social_ado' 
+
+};
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.port || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
 
-app.get('/',function(req,res){
-  res.send("Hey Ya I am the first GET OF SOCIAL_ADO");
-})
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
-app.listen(3000,function(req,res){
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('view engine','ejs');
+
+app.use(session({secret: 'heyitsmenavdeep'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./routes.js')(app,passport);
+require('./config/passport')(passport);
+
+app.listen(port,function(req,res){
   console.log("Server running at port 3000");
 })
